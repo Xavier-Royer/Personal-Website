954652fc07a2c6fe404a951cb33d9bbfb5ca68fe
Xavier Royer <xav.a.royer@gmail.com>
2026-09-04 11:47:37 -0700

    debug errors

--- bootcamp-milestone-2/src/app/projects/page.tsx
+++ bootcamp-milestone-2/src/app/projects/page.tsx
@@ -2,6 +2,9 @@ import ProjectPreview from '@/src/components/projectPreview';
 import _blogs from '../blogData';
 import getProjects from '../projectData';
 
+// Project data comes from MongoDB and must be fetched at request time on Vercel.
+export const dynamic = "force-dynamic";
+
 export default async function Project() {
   const projects = await getProjects();
   console.log("test");
